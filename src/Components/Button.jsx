19abe0fc2c2371f80f4a9d5751935ad89ffe9d1d
nousseibakaabi <nousseiba.kaabi@esprit.tn>
2025-02-03 16:22:52 +0100

    Update Button.jsx

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ class Button extends Component {
     render() { 
         return (
             <>
-      <Button variant="primary">hello</Button>
+      <Button variant="primary">Incrementer</Button>
       </>
         );
     }
